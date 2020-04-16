4849ae1bcd6208c0b533407226fc249b8677fd57
HACKONDEX <sagatelyan.aa@phystech.edu>
2020-04-17 01:41:20 +0300

    encryptor added

--- cesar.py
+++ cesar.py
@@ -4,12 +4,18 @@ import alphabet
 
 def code_caesar(key, code_string):
     coded_answer = ""
+    letter_type = dict()
     for i in code_string:
-        for letter_type in alphabet.alphabet:
-            if i in letter_type:
-                coded_answer += letter_type[(letter_type[i] + key % letter_type['size']
-                              + letter_type['size']) % letter_type['size']]
-                break
+        if i in alphabet.alphabet[0]:
+            letter_type = alphabet.alphabet[0]
+        elif i in alphabet.alphabet[1]:
+            letter_type = alphabet.alphabet[1]
+        else:
+            coded_answer += i
+            continue
+        coded_answer += letter_type[(letter_type[i] + key % letter_type['size']
+                                     + letter_type['size']) % letter_type['size']]
+
     return coded_answer
 
 
---
+++ encryptor.py
@@ -0,0 +1,29 @@
+import cesar
+import vigenere
+
+
+def chiphrator(input_filename, output_filename, key, any_input_direction,
+               any_output_direction, chipher, enc_dec):
+    
+    if any_input_direction:
+        with open(input_filename, 'r') as input_file:
+            input_string = input_file.read()
+    else:
+        input_string = input()
+
+    if chipher == 'cesar':
+        if enc_dec == 'enc':
+            output_string = cesar.encode_caesar(key, input_string)
+        else:
+            output_string = cesar.decode_caesar(key, input_string)
+    else:
+        if enc_dec == 'enc':
+            output_string = vigenere.encode_vigenere(key, input_string)
+        else:
+            output_string = vigenere.decode_vigenere(key, input_string)
+
+    if any_output_direction:
+        with open(output_filename, 'w') as output_file:
+            output_file.write(output_string)
+    else:
+        print(output_string)
