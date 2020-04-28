d5ed5d72b4225d5fd48dc3e57409b73ff34f4eae
HACKONDEX <sagatelyan.aa@phystech.edu>
2020-04-28 21:26:30 +0300

    subparser

--- encryptor.py
+++ encryptor.py
@@ -1,48 +1,84 @@
 import argparse
 import base_functionals
 
-commands = argparse.ArgumentParser()
-commands.add_argument('command', help='encode|decode|hack|train')
-commands.add_argument('--cipher', help='caesar|vigenere')
-commands.add_argument('--key', help='key == number|word')
-commands.add_argument('--input-file', help='file with text to encode')
-commands.add_argument('--output-file', help='the output file')
-commands.add_argument('--text-file', help='file to make model from')
-commands.add_argument('--model-file', help='file to make model there')
-args = commands.parse_args()
 
-try:
-    with open(args.input_file, 'r') as f:
-        a = f.read()
-    any_input_direction = True
-    input_filename = args.input_file
-except:
-    any_input_direction = False
-    input_filename = "file.nothing"
-
-any_output_direction = False
-output_filename = "file.nothing"
-try:
-
-    if not type(args.output_file) == type(None):
-        any_output_direction = True
-        output_filename = args.output_file
+def input_file_checker(args_input_file, file_name):
+    if args_input_file:
+        try:
+            with open(args_input_file.name, 'r') as f:
+                a = f.read()
+            return_ = (True, args_input_file.name)
+        except FileNotFoundError:
+            print(" There is no such", file_name, "file ")
+            raise FileNotFoundError
     else:
-        any_output_direction = False
-        output_filename = "file.nothing"
-except:
-    any_output_direction = False
-    output_filename = "file.nothing"
+        return_ = (False, "no_file")
+    return return_
 
-if args.command == 'train':
-    try:
-        with open(args.text_file, 'r') as f:
-            a = f.read()
-        any_input_direction = True
-        input_filename = args.text_file
-    except:
-        any_input_direction = False
-        input_filename = "file.nothing"
 
-base_functionals.call_right_method(args.command, args.cipher, args.key, any_input_direction,
-                                   any_output_direction, input_filename, output_filename, args.model_file)
+def output_file_checker(args_output_file):
+    if args_output_file:
+        return_ = (True, args_output_file.name)
+    else:
+        return_ = (False, "no_file")
+    return return_
+
+
+def call_cipher(args, enc_dec):
+    input_ = input_file_checker(args.input_file, "input")
+    output_ = output_file_checker(args.output_file)
+    base_functionals.ciphrator(input_[1], output_[1], args.key, input_[0],
+                               output_[0], args.cipher, enc_dec)
+
+
+def encode(args):
+    call_cipher(args, 'enc')
+
+
+def decode(args):
+    call_cipher(args, 'dec')
+
+
+def train(args):
+    input_ = input_file_checker(args.text_file, "text")
+    base_functionals.make_model(input_[1], input_[0], args.model_file.name)
+
+
+def hack(args):
+    input_ = input_file_checker(args.input_file, "input")
+    output_ = output_file_checker(args.output_file)
+    model_ = input_file_checker(args.model_file, "model")
+    base_functionals.hack_cesar(input_[1], output_[1], input_[0], output_[0], model_[1])
+
+
+parser = argparse.ArgumentParser(description=" Command line arguments reader",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+subparsers = parser.add_subparsers()
+parser_encode = subparsers.add_parser('encode', help=' encode command ')
+parser_encode.set_defaults(mode='encode', function=encode)
+parser_encode.add_argument('--cipher', choices=['caesar', 'vigenere'], help=" type of cipher ", required=True)
+parser_encode.add_argument('--key', help=" key of cipher ", required=True)
+parser_encode.add_argument('--input-file', type=argparse.FileType('r'), help=" input file ")
+parser_encode.add_argument('--output-file', type=argparse.FileType('w'), help=' output file ')
+
+parser_decode = subparsers.add_parser('decode', help=' decode command')
+parser_decode.set_defaults(mode='decode', function=decode)
+parser_decode.add_argument('--cipher', choices=['caesar', 'vigenere'], help=' type o cipher ', required=True)
+parser_decode.add_argument('--key', help='Cipher key', required=True)
+parser_decode.add_argument('--input-file', type=argparse.FileType('r'), help=' input file ')
+parser_decode.add_argument('--output-file', type=argparse.FileType('w'), help=' output file ')
+
+parser_train = subparsers.add_parser('train', help=' train command')
+parser_train.set_defaults(mode='train', function=train)
+parser_train.add_argument('--text-file', type=argparse.FileType('r'), help=' input file ')
+parser_train.add_argument('--model-file', type=argparse.FileType('w'), help=' model(output) file', required=True)
+
+parser_hack = subparsers.add_parser('hack', help=' hack command ')
+parser_hack.set_defaults(mode='hack', function=hack)
+parser_hack.add_argument('--cipher', choices=['caesar'], help=' type of cipher ', required=True)
+parser_hack.add_argument('--input-file', type=argparse.FileType('r'), help=' input file ')
+parser_hack.add_argument('--output-file', type=argparse.FileType('w'), help=' output file ')
+parser_hack.add_argument('--model-file', type=argparse.FileType('r'), help=' real model file ', required=True)
+
+commands = parser.parse_args()
+commands.function(commands)
