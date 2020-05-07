31607c688bbb3239da17a3a70305aead91493e26
HACKONDEX <sagatelyan.aa@phystech.edu>
2020-05-07 21:55:35 +0300

    variable bool

--- alphabet.py
+++ alphabet.py
@@ -1,18 +1,16 @@
 import string
 
 
-def built_alphabet(string_, mode):
+def built_alphabet(string_, need_dict_reversed):
     dictionary = dict()
-    key = mode - 1
-    value = 2 - mode
     for i in enumerate(string_):
-        dictionary[i[key]] = i[value]
+        dictionary[i[int(not need_dict_reversed)]] = i[1 - int(not need_dict_reversed)]
 
     return dictionary
 
 
-lowercase_dict = built_alphabet(string.ascii_lowercase, 1)
-uppercase_dict = built_alphabet(string.ascii_uppercase, 1)
-lowercase = built_alphabet(string.ascii_lowercase, 2)
-uppercase = built_alphabet(string.ascii_uppercase, 2)
+lowercase_dict = built_alphabet(string.ascii_lowercase, True)
+uppercase_dict = built_alphabet(string.ascii_uppercase, True)
+lowercase = built_alphabet(string.ascii_lowercase, False)
+uppercase = built_alphabet(string.ascii_uppercase, False)
 size = 26
